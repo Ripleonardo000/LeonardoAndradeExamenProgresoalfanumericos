b089509e7294c8f38a487c4ae0b9f0227d7471e1
cisco <cisco@UPOAULA10309>
2024-06-05 10:42:31 -0500

    Agregar archivos de proyecto.

---
+++ LeonardoAndradeExamenProgresoalfanumericos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LeonardoAndradeExamenProgresoalfanumericos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
